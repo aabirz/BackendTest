524ce4be50047b3b7e82ba5e19a59de24a4015b8
Aabir <abahaida@pca.ma>
2019-02-13 22:47:54 +0100

    Add component to fetch data from API

--- SportsEventsAPI/react-client/src/App.js
+++ SportsEventsAPI/react-client/src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router';
+import { FetchData } from './components/FetchData';
 import './App.css';
 
 class App extends Component {
+  displayName = App.name
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <FetchData/>
         </header>
       </div>
     );
---
+++ SportsEventsAPI/react-client/src/components/FetchData.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+
+export class FetchData extends Component {
+    displayName = FetchData.name
+
+    constructor(props) {
+        super(props);
+        this.state = { events: [], loading: true };
+
+        fetch('http://localhost:9462')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ events: data, loading: false });
+            });
+    }
+
+
+
+    static renderEventsTable(events) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Event Name</th>
+                        <th>Result</th>
+                        <th>Start Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {events.map(elt =>
+                        <tr key={elt.id}>
+                            <td>{elt.name}</td>
+                            <td>{elt.result}</td>
+                            <td>{elt.startDate}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : FetchData.renderEventsTable(this.state.events);
+
+        return (
+            <div>
+                <h1>Fitogram Backend Test</h1>
+                <p>The backend task is to create a Web API using .net core to return a list of sports events of a provider..</p>
+                {contents}
+            </div>
+        );
+    }
+}
