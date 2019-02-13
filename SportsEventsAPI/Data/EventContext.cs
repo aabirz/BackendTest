e52329b5af2948f37e0a0e9b1f41d0141b77ee11
Aabir <abahaida@pca.ma>
2019-02-13 21:57:08 +0100

    Add SeedData Migration

--- SportsEventsAPI/Data/EventContext.cs
+++ SportsEventsAPI/Data/EventContext.cs
@@ -13,11 +13,18 @@ namespace SportsEventsAPI.Data
             : base(options)
         { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<Event>().HasData(
+                    new Event() { Id = 1, Name = "Manchester United VS Paris Saint-Germain", StartDate = new DateTime(2019, 2, 12), EndDate = new DateTime(2019, 2, 12), Result = "0-2" },
+                    new Event() { Id = 2, Name = "Roma VS Paris FC Porto", StartDate = new DateTime(2019, 2, 12), EndDate = new DateTime(2019, 2, 12), Result = "2-1" },
+                    new Event() { Id = 3, Name = "FC Viktoria Plzen VS Roma", StartDate = new DateTime(2018, 12, 12), EndDate = new DateTime(2018, 12, 12), Result = "2-1" }
+            );
+        }
+
         public DbSet<Event> Events { get; set; }
     }
-
-   
-
-   
-
 }
---
+++ SportsEventsAPI/Migrations/20190213205439_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsEventsAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "EndDate", "Name", "Result", "StartDate" },
+                values: new object[] { 1, new DateTime(2019, 2, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Manchester United VS Paris Saint-Germain", "0-2", new DateTime(2019, 2, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "EndDate", "Name", "Result", "StartDate" },
+                values: new object[] { 2, new DateTime(2019, 2, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Roma VS Paris FC Porto", "2-1", new DateTime(2019, 2, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "EndDate", "Name", "Result", "StartDate" },
+                values: new object[] { 3, new DateTime(2018, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "FC Viktoria Plzen VS Roma", "2-1", new DateTime(2018, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
