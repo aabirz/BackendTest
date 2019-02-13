4984170f50337bf4a055efb6b63d9f212e85bd3f
Aabir <abahaida@pca.ma>
2019-02-13 20:40:16 +0100

    Add Service

---
+++ SportsEventsAPI/Services/EventService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsEventsAPI.Models;
+
+namespace SportsEventsAPI.Services
+{
+    public class EventService : IEventService
+    {
+        public List<Event> GetAllEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SportsEventsAPI/Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using SportsEventsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsEventsAPI.Services
+{
+    interface IEventService
+    {
+        List<Event> GetAllEvents();
+    }
+}
