f8c93471909139c0fc5dbcb51cd50f0d88c28158
Aabir <abahaida@pca.ma>
2019-02-13 21:02:02 +0100

    Implement GetAllEvents

--- SportsEventsAPI/Controllers/ValuesController.cs
+++ SportsEventsAPI/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsEventsAPI.Models;
+using SportsEventsAPI.Services;
 
 namespace SportsEventsAPI.Controllers
 {
@@ -10,36 +12,18 @@ namespace SportsEventsAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private IEventService eventService;
+        public ValuesController(IEventService _eventService)
+        {
+            eventService = _eventService;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Event>> GetAllEvents()
         {
-            return new string[] { "value1", "value2" };
+            var events = eventService.GetAllEvents();
+            return Ok(events);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
--- SportsEventsAPI/Services/IEventService.cs
+++ SportsEventsAPI/Services/IEventService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SportsEventsAPI.Services
 {
-    interface IEventService
+    public interface IEventService
     {
         List<Event> GetAllEvents();
     }
